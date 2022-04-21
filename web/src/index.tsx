4ca89d8a17dd3c5c6e6d9e61596f7b2a526154b4
Ted Callahan <tedcallahan@Teds-MBP.hsd1.wa.comcast.net>
2022-04-21 16:09:25 -0700

    Web retrieves info from data server.

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -3,13 +3,62 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  // useQuery,
+  gql,
+  useQuery
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache()
+});
+
+const GET_BOOKS = gql`
+  query GetBooks {
+    books {
+      title
+      author
+    }
+  }
+`
+
+type Book = {
+  author: String;
+  title: String;
+}
+
+type GetBooksResponse = {
+  books: Book[];
+}
+
+function GetBooks() {
+  const {loading, error, data} = useQuery<GetBooksResponse>(GET_BOOKS);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+  if (data) {
+    return <>
+      {data.books.map(({title, author}) => {
+        return <p><i>{title}</i> by {author}</p>
+      })}
+    </>
+  } else {
+    return <p>No Data.</p>
+  }
+}
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+      <GetBooks />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
